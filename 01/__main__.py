3030d42ccb7a7f58777ba6f721c6daf78d3276bc
crsayen <crsayen@gmail.com>
2022-12-01 00:21:35 -0500

    day 1

--- 01/__main__.py
+++ 01/__main__.py
@@ -1,2 +1,11 @@
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    elves = [[]]
+    for line in f:
+        if line == '\n':
+            elves.append([])
+        else:
+            elves[-1].append(int(line.strip()))
+
+srtd = sorted([sum(elf) for elf in elves])
+print('part 1:', srtd[-1])
+print('part 2:', sum(srtd[-1:-4:-1]))
