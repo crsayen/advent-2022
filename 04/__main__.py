28367f83a282815325172947451c16d671d98432
crsayen <crsayen@gmail.com>
2022-12-04 00:46:47 -0500

    add day 4

--- 04/__main__.py
+++ 04/__main__.py
@@ -1,2 +1,18 @@
+contain = 0
+overlap = 0
+
+
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    for line in f:
+        print(line)
+        A, B = [[int(ch) for ch in h.split('-')] for h in line.strip().split(',')]       
+
+        al = list(range(A[0], A[1] + 1))
+        bl = list(range(B[0], B[1] + 1))
+        if len(set([*al, *bl])) == max([len(al), len(bl)]):
+            contain += 1
+        if len(set(al) & set(bl)):
+            overlap += 1
+
+print('part 1:', contain)
+print('part 2:', overlap)
