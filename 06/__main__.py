cc6dd8a7217968b954853756e60f3f4cf581f712
crsayen <crsayen@gmail.com>
2022-12-06 00:26:26 -0500

    add day 6

--- 06/__main__.py
+++ 06/__main__.py
@@ -1,2 +1,12 @@
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    inp = f.read()
+
+
+def findseq(s, l):
+    for i, _ in enumerate(s):
+        if len(set(s[i:i+l])) == l:
+            return i + l
+
+
+print('part 1:', findseq(inp, 4))
+print('part 2:', findseq(inp, 14))
