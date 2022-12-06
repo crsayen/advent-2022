050013c5674634332d12784e404a296298c4bc41
crsayen <crsayen@gmail.com>
2022-12-05 19:27:37 -0500

    add day 5

--- 05/__main__.py
+++ 05/__main__.py
@@ -1,2 +1,28 @@
-with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+from copy import deepcopy
+crates = [[],[],[],[],[],[],[],[],[]]
+
+with open('./input', 'r') as fe:
+    content = fe.read()
+    
+top, bot = [x.split('\n')[:-1] for x in content.split('\n\n')]
+
+for l in top:
+    for i in range(9):
+        cargo = l[i * 4 + 1]
+        if cargo != ' ': crates[i].append(cargo)
+        
+crates2 = deepcopy(crates)
+
+for l in bot:
+    a,b = l.split(' from ')
+    f,t = b.split(' to ')
+    m = a.split(' ')[1]
+    m,f,t = [int(h) - 1 for h in (m,f,t)]
+    stuff = [crates[f].pop(0) for _ in range(m+1)][::-1]
+    stuff2 = [crates2[f].pop(0) for _ in range(m+1)]
+    crates[t] = [*stuff, *crates[t]]
+    crates2[t] = [*stuff2, *crates2[t]]
+
+print('part 1:', ''.join([c[0] for c in crates]))
+print('part 2:', ''.join([a[0] for a in crates2]))
+                
