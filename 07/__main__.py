d797575f39965d795fb5c613969f4227c104d3ba
crsayen <crsayen@gmail.com>
2022-12-08 01:48:57 -0500

    add day 7 and 8

--- 07/__main__.py
+++ 07/__main__.py
@@ -1,2 +1,76 @@
+import dpath
+max = 100000
+disk = 70000000
+needed = 30000000
+
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    lines = [line[:-1] for line in f]
+
+sizes = {}
+counted = []
+wd = ''
+fs = {'root': {'_size': 0}}
+
+
+def get(p):
+    return dpath.get(fs, p)
+
+
+def sets(p, v):
+    dpath.new(fs, p, v)
+
+
+def cd(p):
+    global wd
+    if p == '..':
+        wd = '/'.join(wd.split('/')[:-1])
+    elif p == '/':
+        wd = 'root'
+    else:
+        wd = wd + '/' + p
+
+
+def cont(p):
+    a, b = p.split(' ')
+    if a == 'dir':
+        try:
+            dpath.get(wd + '/' + b)
+        except:
+            sets(wd + '/' + b, {'_size': 0})
+    else:
+        if wd + '/' + b in counted:
+            return
+        counted.append(wd + '/' + b)
+        recursiz(wd + '/' + p, int(a))
+
+
+def recursiz(p, n):
+    pth = 'root'
+    for chnk in p.split('/')[1:]:
+        sz = get(pth + '/_size') + n
+        sets(pth + '/_size', sz)
+        sizes[pth] = sz
+        pth += '/' + chnk
+
+
+for line in lines:
+    if line == 'ls':
+        continue
+    if line.startswith('$ cd'):
+        cd(line.split(' ')[2])
+    elif line[0] == '$':
+        continue
+    else:
+        cont(line)
+
+total = 0
+for k, v in sizes.items():
+    if v <= max:
+        total += v
+print('part 1:', total)
+
+
+for sois in sorted(sizes.values()):
+    if sois >= needed - (disk - get('root/_size')):
+        print('part 2:', sois)
+        break
--- 08/__main__.py
+++ 08/__main__.py
@@ -1,2 +1,77 @@
+import json
+
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    tl = [[int(c) for c in r[:-1]] for r in f]
+
+vis = set()
+
+
+def getVis1d(s):
+    mx = -1
+    vals = []
+    for i, x in enumerate(s):
+        d = int(x)
+        if d > mx:
+            mx = d
+            vals.append(i)
+    return vals
+
+
+def conv1d(s, other, xory, start, direction):
+    vals = getVis1d(s)
+    vals2d = []
+    if xory == 'y':
+        vals2d = [f'{start + (val * direction)},{other}' for val in vals]
+    else:
+        vals2d = [f'{other},{start + (val * direction)}' for val in vals]
+    for e in vals2d:
+        vis.add(e)
+
+
+for y, row in enumerate(tl):
+    conv1d(row, y, 'x', 0, 1)
+    conv1d(row[::-1], y, 'x', len(row) - 1, -1)
+
+for x, col in enumerate(list(zip(*tl[::-1]))):
+    conv1d(col[::-1], x, 'y', 0, 1)
+    conv1d(col, x, 'y', len(col) - 1, -1)
+
+print('part 1:', len(vis))
+
+
+def getHeight(x, y):
+    return tl[y][x]
+
+
+def getYs(x, y, asc):
+    e, d = (len(tl), 1) if asc else (-1, -1)
+    if y == e:
+        return []
+    return [getHeight(x, yy) for yy in range(y + d, e, d)]
+
+
+def getXs(x, y, asc):
+    e, d = (len(tl[0]), 1) if asc else (-1, -1)
+    if x == e:
+        return []
+    return [getHeight(xx, y) for xx in range(x + d, e, d)]
+
+def getScore(x, y):
+    prd = 1
+    val = getHeight(x, y)
+    for view in [*[getXs(x, y, b) for b in [True, False]],*[getYs(x, y, b) for b in [True, False]]]:
+        i = 0
+        for tree in view:
+            i += 1
+            if tree >= val: break
+        prd *= i
+    return prd
+
+highest = -1
+for x in range(len(tl[0])):
+    for y in range(len(tl)):
+        score = getScore(x,y)
+        if score > highest:
+            highest = score
+
+print('part 2:', highest)
