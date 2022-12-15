56baa1cf1a6a89fdd17ecc680d04b727bab7e517
crsayen <crsayen@gmail.com>
2022-12-15 17:32:00 -0500

    add day 11 part one

--- 10/__main__.py
+++ 10/__main__.py
@@ -1,2 +1,191 @@
+# input = [
+#     'addx 15',
+#     'addx -11',
+#     'addx 6',
+#     'addx -3',
+#     'addx 5',
+#     'addx -1',
+#     'addx -8',
+#     'addx 13',
+#     'addx 4',
+#     'noop',
+#     'addx -1',
+#     'addx 5',
+#     'addx -1',
+#     'addx 5',
+#     'addx -1',
+#     'addx 5',
+#     'addx -1',
+#     'addx 5',
+#     'addx -1',
+#     'addx -35',
+#     'addx 1',
+#     'addx 24',
+#     'addx -19',
+#     'addx 1',
+#     'addx 16',
+#     'addx -11',
+#     'noop',
+#     'noop',
+#     'addx 21',
+#     'addx -15',
+#     'noop',
+#     'noop',
+#     'addx -3',
+#     'addx 9',
+#     'addx 1',
+#     'addx -3',
+#     'addx 8',
+#     'addx 1',
+#     'addx 5',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx -36',
+#     'noop',
+#     'addx 1',
+#     'addx 7',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx 2',
+#     'addx 6',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx 1',
+#     'noop',
+#     'noop',
+#     'addx 7',
+#     'addx 1',
+#     'noop',
+#     'addx -13',
+#     'addx 13',
+#     'addx 7',
+#     'noop',
+#     'addx 1',
+#     'addx -33',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx 2',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx 8',
+#     'noop',
+#     'addx -1',
+#     'addx 2',
+#     'addx 1',
+#     'noop',
+#     'addx 17',
+#     'addx -9',
+#     'addx 1',
+#     'addx 1',
+#     'addx -3',
+#     'addx 11',
+#     'noop',
+#     'noop',
+#     'addx 1',
+#     'noop',
+#     'addx 1',
+#     'noop',
+#     'noop',
+#     'addx -13',
+#     'addx -19',
+#     'addx 1',
+#     'addx 3',
+#     'addx 26',
+#     'addx -30',
+#     'addx 12',
+#     'addx -1',
+#     'addx 3',
+#     'addx 1',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx -9',
+#     'addx 18',
+#     'addx 1',
+#     'addx 2',
+#     'noop',
+#     'noop',
+#     'addx 9',
+#     'noop',
+#     'noop',
+#     'noop',
+#     'addx -1',
+#     'addx 2',
+#     'addx -37',
+#     'addx 1',
+#     'addx 3',
+#     'noop',
+#     'addx 15',
+#     'addx -21',
+#     'addx 22',
+#     'addx -6',
+#     'addx 1',
+#     'noop',
+#     'addx 2',
+#     'addx 1',
+#     'noop',
+#     'addx -10',
+#     'noop',
+#     'noop',
+#     'addx 20',
+#     'addx 1',
+#     'addx 2',
+#     'addx 2',
+#     'addx -6',
+#     'addx -11',
+#     'noop',
+#     'noop',
+#     'noop'
+# ]
+
 with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+    input = [line[:-1] for line in f]
+
+inst = []
+for line in input:
+    if line == 'noop':
+        inst.append([1, 0])
+    else:
+        inst.append([2, int(line.split(' ')[1])])
+
+
+def getXpos(n): return n % 40
+
+def getYpos(n): return n // 40
+
+def isLit(x, clock):
+    s = getXpos(clock)
+    return x in [s - 2, s - 1, s]
+
+screen = [['.'] * 40 for _ in range(6)]
+
+clock = 0
+x = 1
+sum = 0
+
+interesting = list(range(-20, 221, 40)[1:])
+
+for instruction in inst:
+    cycles, delta = instruction
+    for cycle in range(cycles):
+        clock += 1
+        print(clock, getXpos(clock), getYpos(clock - 1))
+        if isLit(x, clock):
+            screen[getYpos(clock)][getXpos(clock) - 1] = '#'
+        if clock in interesting:
+            print('cycle:', clock, '\tX:', x)
+            sum += x * clock
+    x += delta
+
+print('part 1:', sum)
+print('part 2:')
+print('\n'.join([''.join(row) for row in screen]))
--- 11/__main__.py
+++ 11/__main__.py
@@ -1,2 +1,80 @@
-with open('./input', 'r') as f:
-    lines = [l.strip() for l in f]
+import json
+import math
+partone = True
+# infile = './input'
+infile = './example'
+
+
+def factors(x):
+    return [i for i in range(1, x + 1) if x % i == 0]
+
+
+def parse_operation(operation):
+    operation = operation.replace('Operation: new = old ', '')
+    if operation == '* old':
+        return lambda old: old
+    return lambda old: eval(f"{old} {operation}")
+
+
+assert parse_operation('Operation: new = old * 3')(3) == 9
+assert parse_operation('Operation: new = old * old')(3) == 3
+
+
+def parse_test(test, if_true, if_false):
+    divisor = int(test.split('by ')[1])
+    true_monkey, false_monkey = [int(x.split('monkey ')[1])
+                                 for x in [if_true, if_false]]
+    return lambda x: false_monkey if x % divisor else true_monkey
+
+
+test_true = parse_test('Test: divisible by 20',
+                       'If true: throw to monkey 1', 'If false: throw to monkey 2')(40)
+test_false = parse_test('Test: divisible by 20',
+                        'If true: throw to monkey 1', 'If false: throw to monkey 2')(39)
+assert test_true == 1, f'expected 1, got {test_true}'
+assert test_false == 2, f'expected 1, got {test_false}'
+
+
+def parse_input_chunk(chunk):
+    name_line, item_line, op_line, test_line, if_true, if_false = chunk.split(
+        '\n')
+    return (int(name_line.split(' ')[1][:-1]), {
+        'items': [int(x) for x in item_line.split(': ')[1].split(', ')],
+        'operation': parse_operation(op_line),
+        'test': parse_test(test_line, if_true, if_false),
+        'tally': 0
+    })
+
+
+test_chunk = '''Monkey 0:
+  Starting items: 1, 2
+  Operation: new = old * 2
+  Test: divisible by 2
+    If true: throw to monkey 1
+    If false: throw to monkey 2'''
+
+n, test_parsed = parse_input_chunk(test_chunk)
+assert n == 0
+assert test_parsed['items'] == [1, 2]
+assert test_parsed['operation'](2) == 4
+assert test_parsed['test'](3) == 2
+
+with open(infile, 'r') as f:
+    chunks = f.read().split('\n\n')
+    monkeys = dict([parse_input_chunk(chunk) for chunk in chunks])
+
+for round in range(20 if partone else 10000):
+    print('starting round', round)
+    for n, stuff in monkeys.items():
+        while stuff['items']:
+            stuff['tally'] += 1
+            item = stuff['items'].pop(0)
+            item = stuff['operation'](item)
+            if partone:
+                item = item // 3
+            target = stuff['test'](item)
+            monkeys[target]['items'].append(item)
+
+sorted_monkeys = sorted([m['tally'] for _, m in monkeys.items()], reverse=True)
+
+print(f'part {1 if partone else 2}:', math.prod(sorted_monkeys[:2]))
